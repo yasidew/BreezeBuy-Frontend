82a06d6b8400b02bd60d509ca6b1bf94ffc20f13
Yasitha Dewmin <ydewmin@gmail.com>
2024-09-29 20:22:01 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Layout from './components/layout';
-import Home from './pages/home';
-import Login from './pages/login';
-import Register from './pages/register';
-import Inventory from './pages/inventory';
-import ManageInventory from './pages/manageinventory';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Layout from "./components/layout";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Inventory from "./pages/inventory";
+import ManageInventory from "./pages/manageinventory";
 
 function App() {
   return (
     <Router>
-    <Layout>
-    
-      <Routes>
-        <Route path= "/" element={<Home/>}  />
-        <Route path= "/login" element={<Login/>}  />
-        <Route path= "/register" element={<Register/>}  />
-        <Route path= "/inventory" element={<Inventory/>}  />
-        <Route path= "/inventory/add" element={<ManageInventory/>}  />
-        <Route path= "/inventory/edit/:id" element={<ManageInventory/>}  />
-      </Routes>
-   
-    </Layout>
-
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/inventory/add" element={<ManageInventory />} />
+          <Route path="/inventory/edit/:id" element={<ManageInventory />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
