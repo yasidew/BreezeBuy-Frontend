807aa4c4fcba30983739df21f9153cb784d4e04a
IT21307058 <it21307058@my.sliit.lk>
2024-10-04 20:16:14 +0530

    changes commited

--- src/pages/AssignRole.js
+++ src/pages/AssignRole.js
@@ -7,9 +7,9 @@ import { toast } from 'react-toastify';
 
 const AssignRole = () => {
     const location = useLocation();
-    const navigate = useNavigate();  // Access the state passed via navigate
-    const { username } = location.state || {};  // Destructure to get the username
-    const [role, setRole] = useState('');  // Default value is empty
+    const navigate = useNavigate();  
+    const { username } = location.state || {};  
+    const [role, setRole] = useState('');  
 
     const handleRoleSubmit = async (e) => {
         e.preventDefault();
